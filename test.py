14c86b73e31cf91c86d93f026cd985911e6c68fb
wyf7685 <69091901+wyf7685@users.noreply.github.com>
2023-12-13 22:33:27 +0800

    调试模式全鼠标操作

--- test.py
+++ test.py
@@ -6,8 +6,6 @@ import pyperclip
 from src.const import *
 from src.level import Level, LevelData
 
-MODE_TEXT = ["NONE", "WALL", "GATE", "FOOD"]
-MODE_KEYMAP = {pygame.K_0: 0, pygame.K_1: 1, pygame.K_2: 2, pygame.K_3: 3}
 
 pygame.init()
 pygame.display.set_icon(pygame.image.load(ICONPATH))
@@ -15,19 +13,33 @@ screen = pygame.display.set_mode((606, 636))
 pygame.display.set_caption("Pac-Man - DEBUG")
 pygame.font.init()
 font = pygame.font.Font(FONTPATH, 18)
+clock = pygame.time.Clock()
 
+MODE_TEXT = ["NONE", "WALL", "GATE", "FOOD"]
 data_fp = LEVELPATH / "dev.json"
 data = LevelData.model_validate_json(data_fp.read_text(encoding="utf-8"))
 level = Level(data)
 level.setup()
-pos = (0, 0)
+prev = pos = (0, 0)
 mode = 0
-undo = []
+undo = []  # type: list[int]
 
 
 def reload_level():
     level.setup()
-    data_fp.write_text(data.model_dump_json(indent=2))
+    data_fp.write_text(data.model_dump_json())
+
+
+def container(id: int):
+    match id:
+        case 1:
+            return data.wall
+        case 2:
+            return data.gate
+        case 3:
+            return data.no_food
+        case _:
+            raise
 
 
 while True:
@@ -36,36 +48,24 @@ while True:
             pygame.quit()
             sys.exit(0)
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            prev = pos
-            pos = pygame.mouse.get_pos()
-            pos = pos[0] // 30, pos[1] // 30
-            pyperclip.copy(f"{pos[0]}, {pos[1]}")
-            t = (prev[0], prev[1], pos[0], pos[1])
-            match mode:
-                case 1:
-                    data.wall.append(t)
-                case 2:
-                    data.gate.append(t)
-                case 3:
-                    data.no_food.append(t)
-            if mode:
-                undo.append(mode)
-                mode = 0
-                reload_level()
-        elif event.type == pygame.KEYDOWN:
-            if event.key in MODE_KEYMAP:
-                mode = MODE_KEYMAP[event.key]
-            elif event.key == pygame.K_z:
-                if not undo:
-                    continue
-                match undo.pop():
-                    case 1:
-                        data.wall.pop()
-                    case 2:
-                        data.gate.pop()
-                    case 3:
-                        data.no_food.pop()
-                reload_level()
+            if event.button == pygame.BUTTON_LEFT:
+                prev = pos
+                pos = pygame.mouse.get_pos()
+                pos = pos[0] // 30, pos[1] // 30
+                pyperclip.copy(f"{pos[0]}, {pos[1]}")
+                if mode:
+                    container(mode).append((prev[0], prev[1], pos[0], pos[1]))
+                    undo.append(mode)
+                    mode = 0
+                    reload_level()
+            elif event.button == pygame.BUTTON_RIGHT:
+                if undo:
+                    container(undo.pop()).pop()
+                    reload_level()
+            elif event.button == pygame.BUTTON_WHEELDOWN:
+                mode = (mode + 1) % len(MODE_TEXT)
+            elif event.button == pygame.BUTTON_WHEELUP:
+                mode = (mode - 1) % len(MODE_TEXT)
 
     level.draw(screen)
     text = font.render(f"Pos: {pos}", True, GREEN)
@@ -74,5 +74,5 @@ while True:
     text = font.render(MODE_TEXT[mode], True, YELLOW)
     x = x - 10 - text.get_rect().width
     screen.blit(text, (x, 610))
-
     pygame.display.flip()
+    clock.tick(30)
